9f1c7e38bb0f40c3dd4966eec1e6e18101645472
Therese <thereseskold@live.com>
2023-03-10 15:21:17 +0100

    fixed the styling in the overlay

--- utils.js
+++ utils.js
@@ -8,14 +8,19 @@ button.addEventListener('click', () => {
 	// Skapa alla element
 	const overlay = document.createElement('div')
 	const content = {
-	contentForm: document.createElement('form'),
-	inputHead: document.createElement('input'),
-	inputPlace: document.createElement('input'),
-	inputDate: document.createElement('input'),
-	inputTime: document.createElement('input'),
-	inputEvent: document.createElement('input'),
-	buttonAddEvent: document.createElement('button')
+		contentForm: document.createElement('form'),
+		head: document.createElement('h2'),
+		inputHead: document.createElement('input'),
+		inputPlace: document.createElement('input'),
+		inputDate: document.createElement('input'),
+		inputTime: document.createElement('input'),
+		inputEvent: document.createElement('input'),
+		buttonContainer: document.createElement('div'),
+		buttonAddEvent: document.createElement('button'),
+		buttonCloseOverlay: document.createElement('button')
 	}
+
+
 
 	//Adderar klasser för styling
 	overlay.classList.add('overlay')
@@ -23,27 +28,52 @@ button.addEventListener('click', () => {
 	content.inputHead.classList.add('input-head')
 	content.inputPlace.classList.add('input-place')
 	content.inputEvent.classList.add('input-event')
+	content.inputDate.classList.add('input-date')
+	content.inputTime.classList.add('input-time')
+	content.buttonContainer.classList.add('button-container')
 	content.buttonAddEvent.classList.add('button-add-event')
 	
 	// Innehåll
 	content.inputDate.type = 'date'
 	content.inputTime.type = 'time'
-	content.inputHead.placeholder = 'Ny aktivitet'
-	content.inputPlace.placeholder = 'Lägg till plats'
-	content.inputEvent.placeholder = 'Lägg till påminnelse'
+	content.inputHead.placeholder = '  Ny aktivitet'
+	content.inputPlace.placeholder = '  Lägg till plats'
+	content.inputEvent.placeholder = '  Lägg till påminnelse'
 	content.buttonAddEvent.innerText = 'Lägg till Event'
+	content.head.innerText = 'Lägg till ett event i din kalender'
+	content.buttonCloseOverlay.innerText = 'Stäng'
 
 	// Lägger till i DOM
+	content.contentForm.append(content.head)
 	content.contentForm.append(content.inputHead)
     content.contentForm.append(content.inputPlace)
 	content.contentForm.append(content.inputDate)
 	content.contentForm.append(content.inputTime)
 	content.contentForm.append(content.inputEvent)
-	content.contentForm.append(content.buttonAddEvent)
+	content.buttonContainer.append(content.buttonAddEvent)
+	content.buttonContainer.append(content.buttonCloseOverlay)
+	content.contentForm.append(content.buttonContainer)
+	
 	overlay.append(content.contentForm)
 
 	body.append(overlay)
+	content.buttonAddEvent.addEventListener('click', (event) => {
+		event.preventDefault();
+		const headValue = content.inputHead.value;
+    	const placeValue = content.inputPlace.value;
+    	const dateValue = content.inputDate.value;
+    	const timeValue = content.inputTime.value;
+    	const eventValue = content.inputEvent.value;
+		eventArray.push(headValue, placeValue, dateValue, timeValue, eventValue)
+	})
 
-	eventArray.push(content.inputHead, content.inputPlace, content.inputDate, content.inputTime, content.inputEvent)
+	content.buttonCloseOverlay.addEventListener('click', (event) => {
+		event.preventDefault();
+	
+		overlay.remove();
+	})
+	
+	console.log(eventArray)
 }) 
-console.log(eventArray)
+
+		
